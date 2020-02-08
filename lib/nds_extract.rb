a72a67614147afdfb2cc2e40199f78debfc24ea3
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-08 15:55:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,7 +29,7 @@ def list_of_directors(source)
   name_index = 0
   while name_index < source.length
     director_name = source[name_index][:name]
-
+    
     name_index += 1
   end
   director_name
