0e682ad26cd32ce65881072f2eb4cd1f2a92f845
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-08 16:12:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,7 +43,7 @@ def total_gross(source)
   name_index = 0
   total = 0
   while name_index < source.length do
-    gross = source[gross_index]
+    gross = source[name_index]
     total += gross_for_director(gross)
 
   end
